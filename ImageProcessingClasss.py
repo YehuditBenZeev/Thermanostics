c758208bf80c677b55302f1bf5969ced17883197
MichalBakshi <mbakshi0548@gmail.com>
2020-06-11 13:06:11 +0300

    devide into classes

---
+++ ImageProcessingClasss.py
@@ -0,0 +1,18 @@
+
+class HotSpot:
+    #self.Powerful
+    #self.Location
+    print("The department displays information for a single point")
+class palm:
+    #DescriptivePalm
+    #finger
+    print("Divide the palm into coordinates")
+class ImageProcessing:
+    #self.listofHotSpot
+    #picture
+    def __init__(self, dir):
+        print("hello")
+    #def BGR2GRAY(self):
+
+
+
