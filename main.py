72c5223e7e5d9a8421549cdb5709969c29239939
YehuditBenZeev <yehudit.benzeev@gmail.com>
2020-10-11 11:52:55 +0300

    divided into packages

--- main.py
+++ main.py
@@ -1,24 +1,21 @@
-import Palm as P
-import ImageProcessing
-
+from imageProcessing import CovertGrayScale, Palm as Pn
 
 ####################################################
-### class  Image_Processing ###
-link = 'Im8.jpg'
-a = ImageProcessing.ImageProcessing(link)
-#a = Image_Processing(link)
-#a.black_border()
-#image = a.convert_image_to_gray_scale()
+# ------------ class  Image_Processing ------------------#
+link = 'Im1.jpg'
+a = CovertGrayScale.ImageProcessing(link)
+# a = Image_Processing(link)
+# a.black_border()
+# image = a.convert_image_to_gray_scale()
 image = a.convert_gray_scale()
 #######################################################
-### class  Hot_Spot ###
-#aa = np.array(image)
-#cv2.circle(aa, (50, 350), 4, (255, 0, 0), 2)
-#ImageProcessing.show_pic(aa, "**************")
+# --------------- class  Hot_Spot ------------- #
+# aa = np.array(image)
+# cv2.circle(aa, (50, 350), 4, (255, 0, 0), 2)
+# ImageProcessing.show_pic(aa, "**************")
 
-b = P.Palm(image, link)
-#b.find_max_for_palm()
-#b.try_detect()
+b = Pn.Palm(image, link)
+# b.find_max_for_palm()
+# b.try_detect()
 b.detect_fingers()
-#b.try_detect()
-#print(b.binary_search(638))
+#b.detect_palm_points()
