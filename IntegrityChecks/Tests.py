d5b36642095250de7246626c5e3d9d4c0cd1e97c
MichalBakshi <58485620+MichalBakshi@users.noreply.github.com>
2020-11-20 02:34:27 +0200

    new teat class

---
+++ IntegrityChecks/Tests.py
@@ -0,0 +1,24 @@
+
+class ObjectTest:
+    def __init__(self,bolean,area):
+        self.bolean = bolean
+        self.area = area # example:(1,1) found(1,2)
+
+
+class Tests:
+    def __init__(self):
+        self.list = []
+        # appending instances to list
+        #read from file exel x,y
+        #list.append(ObjectTest(false, 1))
+    def check_contain(self,listAlgoritm):
+        #open file to read point
+        for i in listAlgoritm:
+            if i in listAlgoritm:
+                self.list.append(0,1) #if point that algoritem found in area so bolean=true
+            else:
+                self.list.append(1,0) # bolean= false
+    def Checking_successes(self):
+        print("")
+
+
