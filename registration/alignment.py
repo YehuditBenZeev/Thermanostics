b4ad1552f280ad5dc18a63f5808efd03ec99f226
YehuditBenZeev <yehudit.benzeev@gmail.com>
2021-02-07 13:53:15 +0200

    image registration, sift doesnt work

--- registration/alignment.py
+++ registration/alignment.py
@@ -1,107 +1,116 @@
 from __future__ import print_function
-
 import cv2
 import numpy as np
 from matplotlib import pylab as plt
-
+import os
+import glob
+import akaze
 
 MAX_MATCHES = 1000
-GOOD_MATCH_PERCENT = 0.01
+GOOD_MATCH_PERCENT = 0.2
 
 
-def alignImages(im1, im2):
-    # Convert images to grayscale
-    plt.figure()
-    a = plt.subplot(2, 2, 1)
-
-    im1Gray = cv2.cvtColor(im1, cv2.COLOR_BGR2GRAY)
-    im2Gray = cv2.cvtColor(im2, cv2.COLOR_BGR2GRAY)
-
-    plt.title("im1Gray")
-    plt.imshow(im1Gray, cmap=plt.get_cmap('gray'))
-    plt.subplot(2, 2, 2, sharex=a, sharey=a)
-    plt.title("im2Gray")
-    plt.imshow(im2Gray, cmap=plt.get_cmap('gray'))
-
-    # Detect ORB features and compute descriptors.
-    # orb = cv2.ORB_create(MAX_MATCHES)
-    akaze = cv2.AKAZE_create()
-    # kp, descriptor = akaze.detectAndCompute(gray, None)
-    # SURF
-    # sift = cv2.SIFT_create()
-    # surf = cv2.SURF(400)
-    keypoints1, descriptors1 = akaze.detectAndCompute(im1Gray, None)
-    keypoints2, descriptors2 = akaze.detectAndCompute(im2Gray, None)
-    print(len(descriptors2), len(descriptors1))
-    # Match features.
-    matcher = cv2.DescriptorMatcher_create(cv2.DESCRIPTOR_MATCHER_BRUTEFORCE_HAMMING)
-    matches = matcher.match(descriptors1, descriptors2, None)
-    print(matches)
-
-    bf = cv2.BFMatcher()
-    # matches = bf.knnMatch(descriptors1, descriptors2, k=2)
-
-    # # Sort matches by score
-    matches.sort(key=lambda x: x.distance, reverse=False)
-
-    # Remove not so good matches
-    numGoodMatches = int(len(matches) * GOOD_MATCH_PERCENT)
-    matches = matches[:numGoodMatches]
-
-    # good_matches = []
-    # for m, n in matches:
-    # if m.distance < GOOD_MATCH_PERCENT * n.distance:
-    # good_matches.append([m])
-
-    print(len(matches))
-    # Draw top matches
-    imMatches = cv2.drawMatches(im1, keypoints1, im2, keypoints2, matches, None)
-    cv2.imwrite("matches.jpg", imMatches)
-    # plt.subplot(2, 2, 3, sharex=a, sharey=a)
-    plt.figure()
-    plt.title("imMatches")
-    plt.imshow(imMatches, cmap=plt.get_cmap('gray'))
-    plt.show(block=True)
-
-    # Extract location of good matches
-    points1 = np.zeros((len(matches), 2), dtype=np.float32)
-    points2 = np.zeros((len(matches), 2), dtype=np.float32)
-
-    for i, match in enumerate(matches):
-        points1[i, :] = keypoints1[match.queryIdx].pt
-        points2[i, :] = keypoints2[match.trainIdx].pt
-
-    # Find homography
-    h, mask = cv2.findHomography(points1, points2, cv2.RANSAC)
-
-    # Use homography
-    height, width, channels = im2.shape
-    im1Reg = cv2.warpPerspective(im1, h, (width, height))
-
-    return im1Reg, h
+def try_():
+    source = np.array([[0, 0], [100, 0], [100, 100], [0, 100]])
+    dest = np.array([[0, 0], [-1000, 0], [-1000, -1000], [0, -1000]])
+    points = np.float32([[[50, 50]]])
+    homography, _ = cv2.findHomography(source, dest)
+    print(source.shape)
+    print(homography.shape)
+    print(homography)
+    print(type(homography))
+    print(type(homography[0, 0]))
+    transformed = cv2.perspectiveTransform(points, homography)
+    print(transformed)
+    # => [[[-500. -500.]]]
+    homography_inverse = np.linalg.inv(homography)
+    detransformed = cv2.perspectiveTransform(transformed, homography_inverse)
+    print(detransformed)
+    # => [[[50. 50.]]]
 
 
-if __name__ == '__main__':
-    # Read reference image
-    refFilename = "0.jpg"
-    print("Reading reference image : ", refFilename)
-    imReference = cv2.imread(refFilename, cv2.IMREAD_COLOR)
+def find_points(points, homography):
+    try:
+        print(points.shape)
+        print(homography.shape)
+        # homography_inverse = np.linalg.inv(homography)
+        transformed = cv2.perspectiveTransform(points, homography)
+        print(transformed)
+    except Exception as e:
+        print(e)
 
-    # Read image to be aligned
-    imFilename = "0_2.jpg"
-    print("Reading image to align : ", imFilename)
-    im = cv2.imread(imFilename, cv2.IMREAD_COLOR)
+        print("fail")
+    return transformed
+
+
+def main(pic_number, image_link):
+    ref_image_link = ''
+    if 'RF' in image_link:
+        ref_image_link = "../images/514 RF.bmp"
+    elif 'RB' in image_link:
+        ref_image_link = "../images/514 RB.bmp"
+    if 'LF' in image_link:
+        ref_image_link = "../images/514 LF.bmp"
+    if 'LB' in image_link:
+        ref_image_link = "../images/514 LB.bmp"
+
+    print("Reading reference image : ", ref_image_link)
+    imReference = cv2.imread(ref_image_link, cv2.IMREAD_COLOR)
+    print("Reading image to align : ", image_link)
+    im = cv2.imread(image_link, cv2.IMREAD_COLOR)
 
     print("Aligning images ...")
     # Registered image will be resotred in imReg.
     # The estimated homography will be stored in h.
-    imReg, h = alignImages(im, imReference)
+    imReg, homography = akaze.align_images(imReference, im)
 
+    points = np.float64([[[215, 36], [44, 153], [19, 225], [55, 270], [105, 303]]])
+    transformed = find_points(points, homography)
     # Write aligned image to disk.
     outFilename = "aligned.jpg"
     print("Saving aligned image : ", outFilename)
     cv2.imwrite(outFilename, imReg)
 
     # Print estimated homography
-    print("Estimated homography : \n", h)
-    
+    print("Estimated homography : \n", homography)
+    return transformed
+
+
+def sow_points_on_image(fig_num, image, image_path, points):
+    print(points)
+    plt.figure(fig_num).clf()
+    plt.title(image_path)
+    plt.imshow(image)
+    labels = set()
+    plt.plot(points[0, :, 0], points[0, :, 1], 'ro', color='r', markersize=4)
+
+
+def run_test():
+    image_list = glob.glob(os.path.join("../514 images", '*.bmp'))
+    for i, image_path in enumerate(image_list):
+        image = plt.imread(image_path)
+        transformed = main(i, image_path)
+        sow_points_on_image(i, image, image_path, transformed)
+    plt.show(block=True)
+
+    # image_path = '../514 images/514 RF flip.bmp'
+    # image = plt.imread(image_path)
+    # transformed = main(0, image_path)
+    # sow_points_on_image(0, image, image_path, transformed)
+    # plt.show(block=True)
+
+
+if __name__ == '__main__':
+    # Read reference image
+    # refFilename = "../images/514 RF.bmp"
+    # print("Reading reference image : ", refFilename)
+    # imReference = cv2.imread(refFilename, cv2.IMREAD_COLOR)
+
+    # Read image to be aligned
+    #main("../514 images/514 RF.bmp")
+    # try_()
+    # po = np.float64([[[215.,  36.], [44., 153.], [19., 225.], [55., 270.], [105., 303.]]])
+    # print("1: \n", po[0])
+    # print("2: \n", po[0, :, 0])
+    # print("3: \n", po[0, :, 1])
+    run_test()
