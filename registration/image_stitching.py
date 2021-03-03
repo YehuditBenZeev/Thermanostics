202e7719d687a2f46de1b27486c506e1adbdaf40
YehuditBenZeev <yehudit.benzeev@gmail.com>
2021-03-03 11:54:05 +0200

    harris corner detection

--- registration/image_stitching.py
+++ registration/image_stitching.py
@@ -7,7 +7,7 @@ def harris_corner_detection():
     img = cv.imread(file_name)
     gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
     gray = np.float32(gray)
-    dst = cv.cornerHarris(gray, 9, 3, 0.04)
+    dst = cv.cornerHarris(gray, 9, 3, 0.06)
 
     #result is dilated for marking the corners, not important
     dst = cv.dilate(dst, None)
@@ -18,3 +18,18 @@ def harris_corner_detection():
     if cv.waitKey(0) & 0xff == 27:
         cv.destroyAllWindows()
 
+    h, w, c = img.shape
+    points = []
+    for i in range(h - 1):
+        for j in range(w - 1):
+            if img[i][j][0] == [0] and img[i][j][1] == [0] and img[i][j][2] == [255]:
+                points.append((i, j))
+
+    # points = [x for i in img if img[x] == [0, 0, 255]]
+    # print("points:")
+    # print(len(points))
+    # print(points)
+    return points
+
+
+harris_corner_detection()
